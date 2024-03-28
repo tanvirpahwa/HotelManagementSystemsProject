b87526919e53554ba6f68295bfdb4e9753bf4815
Tanvir <78670836+tanvirpahwa@users.noreply.github.com>
2024-03-27 23:46:47 -0400

    first commit

---
+++ HotelManagementProject/src/main/java/controller/CustomerController.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * 
+ */
+public class CustomerController {
+
+}
---
+++ HotelManagementProject/src/test/java/com/webdevuoit3230/HotelManagementProject/HotelManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdevuoit3230.HotelManagementProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
