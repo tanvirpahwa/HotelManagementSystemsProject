9f1bdb200091d594343b6233413255013098aa08
Tanvir <78670836+tanvirpahwa@users.noreply.github.com>
2024-04-03 21:01:27 -0400

    Added content into the html files

--- HotelManagementProject/src/main/java/controller/CustomerController.java
+++ HotelManagementProject/src/main/java/controller/CustomerController.java
@@ -5,14 +5,23 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import service.CustomerService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 
  */
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
+
+    @GetMapping
+    public String showCustomer () {
+        return "customer";
+    }
+    
     @Autowired
     private CustomerService customerService;
     public CustomerController(CustomerService customerService) {
