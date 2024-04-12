cbe2ee3c7df2d79ab971ce959a5e321fb87cb0ce
Tanvir <78670836+tanvirpahwa@users.noreply.github.com>
2024-04-11 23:08:41 -0400

    Final Commit

--- HotelManagementProject/src/main/java/com/webdevuoit3230/HotelManagementProject/HotelManagementProjectApplication.java
+++ HotelManagementProject/src/main/java/com/webdevuoit3230/HotelManagementProject/HotelManagementProjectApplication.java
@@ -2,10 +2,8 @@ package com.webdevuoit3230.HotelManagementProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-//@EntityScan("com.example.model")
 public class HotelManagementProjectApplication {
 
 	public static void main(String[] args) {
